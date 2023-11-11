12b2c467f06b3034ebe0b887a47d1e325ece4d44
anahitabozorgi <95561626+anahitabozorgi@users.noreply.github.com>
2023-11-11 14:21:41 +0330

    Add files via upload

--- App/Models/Database.php
+++ App/Models/Database.php
@@ -1,1 +1,33 @@
+<?php
+namespace App\Models; 
 
+use \PDO;
+use \PDOException;
+
+class Database
+{
+    private $conn;
+    private static $instance; 
+
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    } 
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+    
+}
